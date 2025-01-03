f1212c954b816b424256527215f178585a6bc12d
rachelw106 <rachelw106@nycstudents.net>
2025-01-03 14:31:46 +0000

    moveCandyToFirstRow

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -1,1 +1,44 @@
+public class BoxOfCandy
+{
 
+
+    /** box contains at least one row and is initialized in the constructor. */
+    private Candy[][] box;
+
+    public BoxOfCandy(Candy[][] b)
+    {
+        box = b;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for(int row = 0; row < box.length; row++)
+        {
+            for(int col=0;col<box[0].length; col++)
+            {
+                s += box[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    /**
+    * Moves one piece of candy in column col, if necessary and possible, so that the box
+    * element in row 0 of column col contains a piece of candy, as described in part (a).
+    * Returns false if there is no piece of candy in column col and returns true otherwise.
+    * Precondition: col is a valid column index in box.
+    */
+    public boolean moveCandyToFirstRow(int col)
+    { /* to be implemented in part (a) */ }
+
+    /**
+    * Removes from box and returns a piece of candy with flavor specified by the parameter, or
+    * returns null if no such piece is found, as described in part (b)
+    */
+    public Candy removeNextByFlavor(String flavor)
+    { /* to be implemented in part (b) */ }
+
+    // There may be instance variables, constructors, and methods that are not shown.
+}
---
+++ Candy.java
@@ -0,0 +1,20 @@
+public class Candy {
+    private String flavor;
+
+    public Candy(String f) 
+    {
+        flavor = f;
+    }
+
+    public String toString()
+    {
+        return flavor;
+    }
+
+    /** Returns a String representing the flavor of this piece of candy */
+    public String getFlavor() {
+        /* implementation not shown */ }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
